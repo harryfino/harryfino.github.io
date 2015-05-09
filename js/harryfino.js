94c8ab632acd27d8de0006a496e76aa81ecfcd72
Harry Finocchiaro <harry@harryfino.com>
2015-05-09 01:51:55 -0400

    Added click tracking to the external links, updated jQuery

--- js/harryfino.js
+++ js/harryfino.js
@@ -1,3 +1,13 @@
 $(function() {
-	$('#email').attr('href', 'mailto:harry@harryfino.com');
+	$('.js-trackevent').on('click', function (e) {
+		var href = $(this).attr('href');
+		
+		ga('send', 'event', 'outbound', 'click', href, {
+			'hitCallback': function () {
+				document.location = href;
+			}
+		});
+		
+		e.preventDefault();
+	});
 });
